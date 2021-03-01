34c189a01cf5e69e2bd8d38c1e8051f53b14ea39
flavian-anselmo <flaviananselmo6@gmail.com>
2021-03-02 00:32:34 +0300

    Added my project

--- form_handling/form_validation.php
+++ form_handling/form_validation.php
@@ -8,12 +8,66 @@
 </head>
 <body>
     <h1>Form validation</h1>
-    <form action="" method="post">
-        <p> <input type="text" name="name" value="name"> </p>
-        <p> <input type="mail" name="mail" value="mail"> </p>
-        <p> <input type="comment" name="comment" value="comment"> </p>
+    <form action="<?php echo  htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <p> name<input type="text" name="name" value="name"><span>*required<?php echo $namerr;?></span> </p>
+        <p> mail<input type="email" name="mail" value="mail"><span>*required<?php echo $namerr;?> </p>
+        <p> commnts<input type="comment" name="comment" value="comment"> </p>
         <p> <input type="submit" name="submit" value="submit"> </p>
 
     </form>
+    <?php
+        /*
+        form validation with php
+        -empty string
+        -validate string
+        -validate email
+        -validate url
+        -input length
+
+        $_SERVER["PHP_SELF] is a super global variable that returns the file name 
+        of the currently executing script
+        so the $_SERVER["PHP_SELF"] Sends the submitted form data to the page itself 
+        insteadof jumping to a diffrent page the users error msg will appear on the same 
+        form
+        htmlscpecialchars()->function converts special characters HTML entities 
+        this means that it will replace the HTML characters like <>
+        */
+        $name=$email=$comment=$namerr="";//set the variables to empty values 
+        //check for empty strings validate the string 
+        if(!empty($_POST["name"])){
+            $name=$_POST["name"];
+            $nameerr="submitted ";
+           
+        }else{
+           echo "nothing is submitted";
+        }
+        /*
+        in the html form we will add a little script 
+        after each required field which generates the correct
+        error msg if needed 
+        - the next step is to validate the input data that is 
+        does it contain only letters and white spaces 
+        does the email contain email 
+        does the website contain valid url 
+
+        */
+        //validate email
+        ///VALIDATE NUMBER
+        if(!empty($_POST["mail"]) && filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)){
+            $email=$_POST["mail"];
+            echo "email ok";
+
+        }else{
+            echo "no email";
+        }
+        //BUTTON VALIDATION
+        if(isset($_POST["submit"])){
+            if($_SERVER["REQUEST_METHOD"]=="POST"){
+                echo "data  has been recieved ";
+            }
+        }else{
+            echo "destroyed ";
+        }
+    ?>
 </body>
 </html>

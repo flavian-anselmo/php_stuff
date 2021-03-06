f329fe01f99ddd0db08c15fda3066f6fef70f129
flavian-anselmo <flaviananselmo6@gmail.com>
2021-03-07 01:09:02 +0300

    Added my project

--- file_handling/cookies.php
+++ file_handling/cookies.php
@@ -41,20 +41,44 @@
         //first check if the submit button is clicked 
         //use the isset($_POST["name"])-> function
         $name=$passwd="";
+        $cookie_name=$cookie_password="";//set the variables to null
         if ($_SERVER["REQUEST_METHOD"]=="POST"){
             //check for the button 
             if (isset($_POST["submit"])){
-            
                 echo "hello web devs";
                 //check for empty inputs and validate the inputs 
                 if (!empty($_POST["username"]) and !empty($_POST["password"])){
                     //validations using regular expressions 
                     if(preg_match("/^[a-z|A-Z]+/i",$_POST["username"]) and preg_match("/\w+/",$_POST["password"])){
                         //check for the length of password 
-                        if(count($_POST["password"])>=8){
+                        if(strlen($_POST["password"])>=8){
                             $name=$_POST["username"];
                             $passwd=$_POST["password"];
                             echo "we have inputs";
+                            //set the cookies if all the conditions are met
+                            /**
+                             * cookies are often used to identify a user
+                             * a small file that that the server emebeds 
+                             * on the users computer
+                             * A cookie iis created ising the setcookie() function
+                             * we create then retrieve a cookie value 
+                             * we then retrieve the value of the cookie using 
+                             * the global variable $_COOKIE ..we can use the 
+                             * isset() function to check if the function 
+                             * is set 
+                             * set the cookie with the setcookie(value,value time ...)
+                             */
+                            #now set the cookies
+                            setcookie($name,$passwd,time()+(86400)*30);// the cookie is set
+                            echo $_COOKIE[$name];
+                            //retrieve the set 
+                            if (!isset($_COOKIE[$name]) and !isset($_COOKIE[$passwd])){
+                                echo "cookies are  not set";
+
+                            }else{
+                                echo "the cookies are set";
+                            }
+
                         }else{
                             echo "weak password";
                         }

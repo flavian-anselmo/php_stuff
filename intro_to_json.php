73470701c09277605e58e85a502b58889e5949c9
Flavian Anselmo <68228894+flavian-anselmo@users.noreply.github.com>
2021-03-09 15:39:29 +0300

    Add files via upload

---
+++ intro_to_json.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * JSON
+ * Java script object notation which is a syntax for storing and exchanging data 
+ * php has some built iin functions to handle json 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
